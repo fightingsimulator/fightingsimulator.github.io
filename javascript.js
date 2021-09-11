76dc637d6dc8543aa284521ad09ba01ba0c6df95
danil123212 <61593693+danil123212@users.noreply.github.com>
2021-09-11 21:11:49 +0300

    web

--- javascript.js
+++ javascript.js
@@ -2,3 +2,7 @@ function onClickFunction() {
   window.open("https://discord.com/invite/F3qHSPsPHc");
   console.log("los");
 }
+function onClickFunction2() {
+  window.open("https://www.roblox.com/games/7453220634/fight-simulator");
+  console.log("los");
+}
